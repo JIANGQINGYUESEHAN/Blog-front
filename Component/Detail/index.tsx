16820f59153fd75ea99e666b3767d02098ed1930
zy <3325246991@qq.com>
2023-10-25 23:11:01 +0800

    classification is over

--- Component/Detail/index.tsx
+++ Component/Detail/index.tsx
@@ -5,6 +5,19 @@ export default function Detail() {
     return (
         <div className={styles.Detail}>
 
+            <div className={styles.more}>
+                <span className='iconfont' style={{ fontSize: '20px' }}>
+                    &#xe68b;
+                </span>&nbsp;
+                <span className={styles.a}>分类</span>
+
+            </div>
+            {/*文章类别 */}
+            <div className={styles.article}>
+                <div className={styles.div1}>
+                    <span> TypeScript </span>&nbsp; <span>(1)</span>
+                </div>
+            </div>
         </ div>
     )
 }
