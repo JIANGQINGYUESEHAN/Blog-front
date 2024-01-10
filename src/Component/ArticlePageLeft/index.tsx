5467297a04b3db8b4d43e46a43f91392a6c2cc16
zy <3325246991@qq.com>
2024-01-10 22:49:08 +0800

    continue

--- src/Component/ArticlePageLeft/index.tsx
+++ src/Component/ArticlePageLeft/index.tsx
@@ -3,6 +3,7 @@ import type { FC, ReactNode } from 'react'
 import styles from './ArticlePage.module.scss'
 import { img } from '@/assets/utils/BaseData'
 import Image from 'next/image';
+import { ScheduleOutlined } from '@ant-design/icons';
 interface IProps {
     children?: ReactNode
 }
@@ -10,13 +11,34 @@ interface IProps {
 const ArticlePageLeft: FC<IProps> = () => {
     return <div className={styles.ArticlePage}>
         <div className={styles.left}>
-            <div className={styles.age}> <Image src={img}
-                width={360}
-                height={300}
-                alt='头像'
-                className={styles.image} /></div>
+            <div className={styles.age}>
+                <Image src={img}
+                    width={360}
+                    height={300}
+                    alt='头像'
+                    className={styles.image} />
+            </div>
         </div>
-        <div className={styles.right}></div>
+        <div className={styles.right}>
+            <div className={styles.card}>
+                <div className={styles.header}>
+
+                    <span className={styles.icon}><ScheduleOutlined rev={undefined} /></span>
+                    <span className={styles.date}>2023-12-25 17:29:30</span>
+
+
+                </div>
+                <div className={styles.Title}>
+                    <span className={styles.Title} >Element-UI 控制台报错 Prop being mutated: "placement"</span>
+                </div>
+                <div className={styles.content}>
+
+                </div>
+                <div className={styles.footer}>
+                    <span className={styles.tag}>VUE</span>
+                </div>
+            </div>
+        </div>
 
 
     </div>
