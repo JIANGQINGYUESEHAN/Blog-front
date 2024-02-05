8ca9035bea27ff1d07e6cc1d6133ee8748cd3344
zy <3325246991@qq.com>
2024-02-05 21:15:49 +0800

    continue

--- src/app/articledetail/[slug]/page.tsx
+++ src/app/articledetail/[slug]/page.tsx
@@ -2,6 +2,7 @@ import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import style from './style.module.scss'
 import { img } from '../../../assets/utils/BaseData';
+import { ScheduleOutlined, UserOutlined } from '@ant-design/icons';
 
 
 interface IProps {
@@ -16,6 +17,24 @@ const ArticleDetail: FC<IProps> = () => {
             <div className={style.articleDetailIcon}>
                 <div className={style.Title}>
                     组件中，我假设了一个简单的结构来匹配截图的内容布局。在实际的组件中，我假设了一个简单的结构来匹配截图的内容布局。在实际的开发中，你可能还开发中，你可能还
+                </div>
+
+                <div className={style.icon}>
+                    <div className={style.iconItem}>
+                        <div className={style.iconCtrl}>
+                            <UserOutlined rev={undefined} />
+                        </div>&nbsp;
+                        <span>zy</span>
+
+                    </div>
+                    <div className={style.dateItem}>
+                        <div className={style.iconCtrl}>
+                            <ScheduleOutlined rev={undefined} />
+                        </div>&nbsp;
+                        <span>2023-12-25 17:29:30</span>
+
+                    </div>
+
                 </div>
             </div>
         </div>
